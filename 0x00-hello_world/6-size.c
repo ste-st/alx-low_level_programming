ad20410eaa24ca1f3f101e909cfb771ee146d8c1
root <as-ine.f@hotmail.com>
2021-06-17 13:57:07 -0700

    fix 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - should Print the size of arious data types
+ * ont the computer.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
@@ -6,6 +13,5 @@ printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 printf("Size of a float: %zu byte(s)\n", sizeof(float));
-
 return (0);
 }

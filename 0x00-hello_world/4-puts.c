59eadd63f8f67dbc0ffadee9197c9ff0eb9557c8
root <as-ine.f@hotmail.com>
2021-06-17 10:55:04 -0700

    Add files 2 4 5 and 6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main -Prints "Programing is like building a
+ *multingual puzzle"
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  puts ("\"Programming is like building a multilingual puzzle");
+  return (0);
+}

8626d62b2b5f114e5ebedb2a50816406a0e088e1
root <as-ine.f@hotmail.com>
2021-06-18 08:20:28 -0700

    fix 1 and add file 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Return: returns always 0 (Success)
+ */
+
+int main(void)
+{
+
+char l;
+for (l = 'a'; l <= 'z'; l++)
+{
+putchat(l);
+}
+putchar(10);
+
+return (0);
+}

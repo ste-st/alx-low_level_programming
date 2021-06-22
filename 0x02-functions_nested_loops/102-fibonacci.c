688574353f9ea6f00363140bba6fc38147b6ab38
root <as-ine.f@hotmail.com>
2021-06-22 11:48:30 -0700

    Fix code

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+long int a = 1, b = 2, c, d;
+for (c = 1; c <= 50; ++c)
+{
+if (a != 20365011074)
+{
+printf("%d, ", a);
+}
+else
+{
+printf("%d, ", a);
+}
+d = a + b;
+a = b;
+b = d;
+}
+return (0);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,13 +14,13 @@ _putchar('0');
 for (b = 1; b <= 9; b++)
 {
 _putchar(',');
-_putchat(' ');
+_putchar(' ');
 c = a * b;
 if (c <= 9)
 _putchar(' ');
 else
 _putchar((c / 10) + '0');
-_putchar((prod % 10) + '0');
+_putchar((c % 10) + '0');
 }
 _putchar('\n');
 }

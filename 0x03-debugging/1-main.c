421d502a0fa562697c2ac4bdf8642abf9bc485f9
root <as-ine.f@hotmail.com>
2021-06-25 04:22:34 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,13 +11,13 @@ int i;
 
 printf("Infinite loop incoming :(\n");
 
-i = 20;
-
-while (i < 10)
-{
-putchar(i);
-}
-
+i = 0;
+/*
+*while (i < 10)
+*{
+*putchar(i);
+*}
+*/
 printf("Infinite loop avoided! \\0/\n");
 
 return (0);

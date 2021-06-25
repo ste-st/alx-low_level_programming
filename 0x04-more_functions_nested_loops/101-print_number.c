e0e7f29fd7288cbd1dc40f9daf70d0a529d42b81
root <as-ine.f@hotmail.com>
2021-06-25 05:21:47 -0700

    Add advanced

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ *print_number - prints an integer
+ * @n: int
+ * Return: void
+ */
+
+void print_number(int n)
+{
+unsigned int m;
+
+if (n < 0)
+{
+m = -n;
+_putchar('_');
+}
+else
+{
+m = n;
+}
+if (m / 10)
+{
+print_number(m / 10);
+}
+_putchar((m % 10) + '0');
+}

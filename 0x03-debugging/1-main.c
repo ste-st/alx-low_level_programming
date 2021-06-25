70da7ff1a9b0c614421fd13efb55961497bc5cdb
root <as-ine.f@hotmail.com>
2021-06-25 04:03:40 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
+
 /**
- *main - Entry point.
- *Return: Always 0.
+ * main - causes an infinite loop
+ * Return: 0
  */
+
 int main(void)
 {
 int i;
+
 printf("Infinite loop incoming :(\n");
+
 i = 20;
+
 while (i < 10)
 {
 putchar(i);
 }
+
 printf("Infinite loop avoided! \\0/\n");
+
 return (0);
 }
